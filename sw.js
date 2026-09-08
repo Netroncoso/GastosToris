362fc00dfde40f5c2785a47dd0a1a88142c3be9e
Netroncoso <net.troncoso@gmail.com>
2026-09-07 21:38:57 -0300

    a

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker liviano: cachea estáticos de GitHub Pages.
    HTML/JS/CSS van network-first para no quedar con versiones viejas en el teléfono. */
-const CACHE = 'torisapp-static-v54';
+const CACHE = 'torisapp-static-v55';
 const PRECACHE = [
   './',
   './index.html',
